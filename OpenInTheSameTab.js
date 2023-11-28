d3a886aa1d2a5416e3f1bf3dec12e172c346cbf1
Neil Hooey <nhooey@gmail.com>
2023-11-29 00:21:31 +0700

    Indent code and remove obvious install instructions.

--- OpenInTheSameTab.js
+++ OpenInTheSameTab.js
@@ -1,19 +1,17 @@
 // get settings
 chrome.storage.sync.get({
-	// default value
-	t1tab: true
-	}, function(items) {
-    t1tab = items.t1tab;
+  // default value
+  t1tab: true
+}, function (items) {
+  t1tab = items.t1tab;
 
-
- 
-if ( t1tab == true ){
-	var a = document.getElementsByTagName("a"); 
-	for (i=0;i<a.length;i++) { 
-        if (a[i].target=="_blank" /* aggressive mode goes here */) { 
-            a[i].target="_top" /* _self vs _top */
-        } 
-	}
-}
+  if (t1tab == true) {
+    var a = document.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      if (a[i].target == "_blank" /* aggressive mode goes here */) {
+        a[i].target = "_top" /* _self vs _top */
+      }
+    }
+  }
 
 });
--- popup.js
+++ popup.js
@@ -3,14 +3,16 @@ function save_options(reload) {
   var t1pop = document.getElementById('cb_t1pop').checked;
   var t1foc = document.getElementById('cb_t1foc').checked;
   var t1tab = document.getElementById('cb_t1tab').checked;
-  
+
   chrome.storage.sync.set({
     t1pop: t1pop,
-	t1foc: t1foc,
+    t1foc: t1foc,
     t1tab: t1tab
-  }, function() {
-	console.log('Options saved');
-	if (reload == true) { chrome.runtime.reload(); }
+  }, function () {
+    console.log('Options saved');
+    if (reload == true) {
+      chrome.runtime.reload();
+    }
 
   });
 }
@@ -19,17 +21,23 @@ function save_options(reload) {
 window.onload = function restore_options() {
   chrome.storage.sync.get({
     // default values
-	t1pop: true,
-	t1foc: true,
+    t1pop: true,
+    t1foc: true,
     t1tab: true
-  }, function(items) {
+  }, function (items) {
     document.getElementById('cb_t1pop').checked = items.t1pop;
-	document.getElementById('cb_t1foc').checked = items.t1foc;
+    document.getElementById('cb_t1foc').checked = items.t1foc;
     document.getElementById('cb_t1tab').checked = items.t1tab;
-	console.log('Options restored');
+    console.log('Options restored');
   });
 }
 
-document.getElementById('cb_t1pop').addEventListener('click', function() {save_options(true);} );
-document.getElementById('cb_t1foc').addEventListener('click', function() {save_options(true);} );
-document.getElementById('cb_t1tab').addEventListener('click', function() {save_options(false);} );
+document.getElementById('cb_t1pop').addEventListener('click', function () {
+  save_options(true);
+});
+document.getElementById('cb_t1foc').addEventListener('click', function () {
+  save_options(true);
+});
+document.getElementById('cb_t1tab').addEventListener('click', function () {
+  save_options(false);
+});
